0652d78aed31dafdba20cfd400d3a13b9930c2cb
majhouldiscord-cmd <majhouldiscord@gmail.com>
2026-06-19 13:56:31 +0100

    Add full auth URL logging

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -3,10 +3,11 @@ exports.handler = async (event, context) => {
   const REDIRECT_URI = process.env.REDIRECT_URI;
   const SCOPES = "identify guilds";
 
-  console.log('DEBUG - CLIENT_ID:', CLIENT_ID ? 'Set' : 'UNDEFINED');
+  console.log('DEBUG - CLIENT_ID:', CLIENT_ID);
   console.log('DEBUG - REDIRECT_URI:', REDIRECT_URI);
 
   const authUrl = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=${SCOPES.replace(/ /g, "%20")}`;
+  console.log('DEBUG - Generated auth URL:', authUrl);
 
   return {
     statusCode: 302,
