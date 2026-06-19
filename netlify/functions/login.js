82a52c3a6b3af0708f41dc2418aa5a79b47c9ea0
majhouldiscord-cmd <majhouldiscord@gmail.com>
2026-06-19 12:41:05 +0100

    Add debug logging for OAuth variables

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -3,6 +3,9 @@ exports.handler = async (event, context) => {
   const REDIRECT_URI = process.env.REDIRECT_URI;
   const SCOPES = "identify guilds";
 
+  console.log('DEBUG - CLIENT_ID:', CLIENT_ID ? 'Set' : 'UNDEFINED');
+  console.log('DEBUG - REDIRECT_URI:', REDIRECT_URI);
+
   const authUrl = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=${SCOPES.replace(/ /g, "%20")}`;
 
   return {
